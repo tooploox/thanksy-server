45127935c71eabc271c7d423697f8afe2b2bc8fe
Tomek Rybczyński <tomek.rybka@gmail.com>
2019-01-24 13:15:41 +0100

    Init main endpoint

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@
 
 require "roda"
 
-class Application < Roda
+class App < Roda
+  plugin :indifferent_params
+  plugin :json
+  plugin :json_parser
+
   route do |r|
+    r.on "thanks", method: :post do |data|
+      puts r.body.read
+    end
   end
 end
--- config.ru
+++ config.ru
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "rack"
 require "./app"
 
-run Application.freeze.app
+run App.freeze.app
