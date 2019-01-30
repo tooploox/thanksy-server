c1a7ef25bbfc760202a8e9da7474f4d704c69ef6
Tomek Rybczyński <tomek.rybka@gmail.com>
2019-01-30 13:00:28 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_121502) do
-
+ActiveRecord::Schema.define(version: 20_190_125_121_502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,5 +38,4 @@ ActiveRecord::Schema.define(version: 2019_01_25_121502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
