3dad497e4d924a9c5e69961a37be5d45fe9d397a
Tomek Rybczyński <tomek.rybka@gmail.com>
2019-01-30 09:06:25 +0100

    Created thanks model and slack user model

---
+++ app/models/slack_user.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class SlackUser < ApplicationRecord
+  self.primary_key = "id"
+end
---
+++ app/models/thanks.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Thanks < ApplicationRecord
+  self.primary_key = "id"
+end
