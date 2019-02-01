96017c5f42945c0d44751c0ca81047d836afcae6
Tomek Rybczyński <tomek.rybka@gmail.com>
2019-02-01 10:09:33 +0100

    Updated rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative "./support/test_helpers"
+require_relative "./support/params_factory"
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -10,4 +13,17 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.include TestHelpers
+  config.include ParamsFactory
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
