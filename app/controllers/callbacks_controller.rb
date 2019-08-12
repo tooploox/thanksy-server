a50a3108fe4c73c8b0d47847fbced44f21196b08
bart <bart.dy@gmail.com>
2019-08-12 13:29:15 +0200

    bump

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class CallbacksController < ApplicationController
-  before_action :verify_slack_token
+  # before_action :verify_slack_token
 
   def process
     puts "AAA"
-    # HandleCallback.new.call(params)
+    HandleCallback.new.(params)
     puts "AAA"
     render json: { text: "ok" }
   end
