cc629ed27d7c9523abbe7a193a80e0d06384821e
bart <bart.dy@gmail.com>
2019-08-12 14:28:07 +0200

    bump

--- app/services/create_post.rb
+++ app/services/create_post.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class CreatePost
-  def call(payload)
+  include SuckerPunch::Job
+
+  def initialize(slack_client = ::Adapters::Slack.new)
+    @slack_client = slack_client
+  end
+
+  def perform(payload)
     puts "add post"
+    giver = find_giver(payload["user"]["name"])
+    puts giver
     puts YAML.dump(payload)
-    payload
+  rescue FindSlackUsers::SlackUserNotFound => e
+    notify_slack_about_error(payload[:response_url], e.message)
   end
 
   private
@@ -16,5 +25,9 @@ class CreatePost
       title: payload["title"],
       text: payload["text"],
     )
+  end
+
+  def notify_slack_about_error(response_url, message)
+    @slack_client.send(response_url, text: message)
   end
 end
