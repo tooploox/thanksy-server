68d3e9320fc1bcbd02082f7eb8e561fe62ad5b84
bart <bart.dy@gmail.com>
2019-08-12 16:31:50 +0200

    bump

--- app/templates/slack_posts_list.rb
+++ app/templates/slack_posts_list.rb
@@ -13,7 +13,7 @@ class SlackPostsList
   def list(posts)
     out = "<b>Active Posts:</b> \n"
     posts.each do |p|
-      out << "#{p.id} | #{p.title} | #{p.publish_start} \n"
+      out += "#{p.id} | #{p.title} | #{p.publish_start} \n"
     end
     out
   end
