ef121de77e2ab284ac7048335ec382519913622a
bart <bart.dy@gmail.com>
2019-08-12 23:29:36 +0200

    bump

--- app/templates/slack_posts_list.rb
+++ app/templates/slack_posts_list.rb
@@ -12,9 +12,9 @@ class SlackPostsList
 
   def list(posts)
     out = "*Active Posts:* \n"
-    out += "ID  |  Title  |  Author  | PublishAt | PublishedTill \n"
+    out += "ID  |  Category | Title  |  Author  | PublishAt | PublishedTill \n"
     posts.each do |p|
-      out += "*#{p.id}* | *#{p.title}* | #{p.author['name']} | #{p.publish_start.strftime('%F %H:%M')} | #{p.publish_end.strftime('%F %H:%M')} \n"
+      out += "#{p.id} | #{p.category} | *#{p.title}* | #{p.author['name']} | #{p.publish_start.strftime('%F %H:%M')} | #{p.publish_end.strftime('%F %H:%M')} \n"
     end
     out
   end
