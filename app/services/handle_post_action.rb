1f6637b84043d63823972ae57b0f3ef0d0f976b6
bart <bart.dy@gmail.com>
2019-08-16 14:53:16 +0200

    bump

--- app/services/handle_post_action.rb
+++ app/services/handle_post_action.rb
@@ -3,8 +3,14 @@
 class HandlePostAction
   def call(payload)
     puts "handle actions"
-    action = payload["actions"].first
-    puts action
-    {}
+    act = payload["actions"].first
+    post = Post.find_by_id(act["value"].to_i)
+    action = act["name"]
+    if action.to_sym == :destroy
+      post.destroy
+      {}
+    else
+      OpenPostDialog.new.edit(payload["trigger_id"], post)
+    end
   end
 end
