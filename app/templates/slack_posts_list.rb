b399b96885549d261a1244ec8229aa094009eb79
bart <bart.dy@gmail.com>
2019-08-16 13:49:26 +0200

    bump

--- app/templates/slack_posts_list.rb
+++ app/templates/slack_posts_list.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
 class SlackPostsList
-
   def call(posts)
     {
-      "text": list(posts),
+      text: list(posts),
+      attachments: [
+        {
+          text: "No helo",
+          fallback: "You are unable to choose a game",
+          callback_id: "post_actions",
+          color: "#e5e5e5",
+          attachment_type: "default",
+          actions: [
+            {
+              name: "love",
+              text: ":heart: lol",
+              type: "button",
+              value: "1234",
+            },
+          ],
+        },
+      ],
     }
   end
 
